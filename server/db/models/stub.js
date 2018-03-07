9d48c3e38421729280ee0c18073becfd4bdbdf43
kyusulamita <sulamitamorales@yahoo.com>
2018-03-06 19:40:25 -0500

    feature: empty components

---
+++ server/db/models/stub.js
@@ -0,0 +1,39 @@
+const crypto = require('crypto')
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Stub = db.define('employee', {
+  rate: {
+    type: Sequelize.STRING,
+    default: 12,
+    allowNull: false
+  },
+  rateType: {
+    type: Sequelize.ENUM('HOURLY, WEEKLY'),
+    default: 'HOURLY'
+  },
+  hours: {
+    type: Sequelize.STRING,
+  },
+  pay: {
+    type: Sequelize.VIRTUAL,
+    get(){
+      const hourlyPay = this.get('rateType') === 'HOURLY';
+      if (hourlyPay){
+        return +this.get('rate') * + this.get('hours');
+      }
+      return +this.get('rate');
+    }
+  },
+  start: {
+    type: Sequelize.STRING,
+    allowNull: false,
+
+  },
+  end: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+})
+
+module.exports = Employee;
