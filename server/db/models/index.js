fd83d2b7738162cb9bfb31911c2d54cdcdcb3723
kyusulamita <sulamitamorales@yahoo.com>
2018-03-06 19:37:17 -0500

    feature: added stub info

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,6 @@
-const User = require('./user')
-
+const User = require('./user');
+const Employee = require('./Employee');
+const Stub = require('stubs');
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
@@ -7,12 +8,19 @@ const User = require('./user')
  *    BlogPost.belongsTo(User)
  */
 
+ Stub.belongsTo(Employee);
+ Employee.hasMany(Stub);
+
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
+
 module.exports = {
-  User
+  User,
+  Employee,
+  Stub
 }
