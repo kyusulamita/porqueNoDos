25f5af8d3faf9cbb77858cc3d86558fb5802b193
kyusulamita <sulamitamorales@yahoo.com>
2018-03-08 23:48:05 -0500

    new components

--- client/components/employeeDetail.js
+++ client/components/employeeDetail.js
@@ -1,3 +1,14 @@
 import React, { Component } from 'react'
 
+class employeeDetail extends Component{
+  constructor(props){
+    super(props);
+    // this.state
+  }
 
+  render() {
+    return(
+      <div>Employee Detail</div>
+    )
+  }
+}
