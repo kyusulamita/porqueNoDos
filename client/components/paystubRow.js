f146d194a207f90ef16a670e21587fec1ef827b9
kyusulamita <sulamitamorales@yahoo.com>
2018-03-08 23:50:55 -0500

    rowStub

--- client/components/paystubRow.js
+++ client/components/paystubRow.js
@@ -1,3 +1,21 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+/**
+ * COMPONENT
+ */
+export const stubRow = (props) => {
+  // const {rate, hours} = props
+
+  return (
+    <div>
+      <h3>Hours</h3>
+      <h3>Rate</h3>
+      <h3>Total</h3>
+    </div>
+  )
+}
 
 
+
+export default stubRow;
